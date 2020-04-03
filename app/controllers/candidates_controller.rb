30eeeec098aedea265f59aba11e6725ba214caad
Shuhan Hu <1119han@gmail.com>
2020-04-03 22:50:04 +0800

    Scss test, edit and delete function done

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -18,10 +18,30 @@ class CandidatesController < ApplicationController
       if @candidate.save
         flash[:notice] = "The candidate has been successfully created."
         redirect_to candidates_path 
- # notice: "The candidate has been successfully created."
       else
         render :new
       end
+    end
+
+    def edit
+      @candidate = Candidate.find_by(id: params[:id])
+    end
+
+    def update
+      @candidate = Candidate.find_by(id: params[:id])
+
+      if @candidate.update(candidate_params)
+        flash[:notice] = "The candidate has been successfully updated."
+        redirect_to candidates_path 
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @candidate = Candidate.find_by(id: params[:id])
+      @candidate.destroy if @candidate
+      redirect_to candidates_path, notice: "The candidate has been successfully delete."
     end
 
     private
