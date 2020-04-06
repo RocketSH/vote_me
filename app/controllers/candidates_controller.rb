ccb710a0c0e35d374b600faed381c7d6ed305bbe
Shuhan Hu <1119han@gmail.com>
2020-04-06 21:34:08 +0800

    Use notice, partial render. Added _ad.html.erb

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,59 +1,57 @@
 # coding: utf-8
 class CandidatesController < ApplicationController
 
-    def index
-      @candidates = Candidate.all
-    end
+  before_action :candidate_id, only: [:show, :edit, :update, :destroy, :vote]
+                               
+  def index
+    @candidates = Candidate.all
+  end
 
-    def show
-     @candidate = Candidate.find_by(id: params[:id])
-    end
+  def show
+  end
 
-    def new
-      @candidate = Candidate.new
-    end
-    
-    def create
-      @candidate = Candidate.new(candidate_params)
+  def new
+    @candidate = Candidate.new
+  end
 
-      if @candidate.save
-        flash[:notice] = "The candidate has been successfully created."
-        redirect_to candidates_path 
-      else
-        render :new
-      end
-    end
+  def create
+    @candidate = Candidate.new(candidate_params)
 
-    def edit
-      @candidate = Candidate.find_by(id: params[:id])
+    if @candidate.save
+      redirect_to candidates_path, notice: "The candidate has been successfully created."
+    else
+      render :new
     end
+  end
 
-    def update
-      @candidate = Candidate.find_by(id: params[:id])
+  def edit
+  end
 
-      if @candidate.update(candidate_params)
-        flash[:notice] = "The candidate has been successfully updated."
-        redirect_to candidates_path 
-      else
-        render :edit
-      end
-    end
+  def update
 
-    def destroy
-      @candidate = Candidate.find_by(id: params[:id])
-      @candidate.destroy if @candidate
-      redirect_to candidates_path, notice: "The candidate has been successfully delete."
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "The candidate has been successfully updated."
+    else
+      render :edit
     end
+  end
 
-    def vote
-      @candidate = Candidate.find_by(id: params[:id])
-      @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate 
-       
-      redirect_to candidates_path, notice: "完成投票!"
-    end
-      
-    private
-    def candidate_params
-      params.require(:candidate).permit(:name, :age, :party, :politics)
-    end
+  def destroy
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: "The candidate has been successfully delete."
+  end
+
+  def vote
+    @candidate.vote_logs.create(ip_address: request.remote_ip) if @candidate 
+    redirect_to candidates_path, notice: "完成投票!"
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
 end
+
+  def candidate_id
+    @candidate ||= Candidate.find_by(id: params[:id])
+  end
